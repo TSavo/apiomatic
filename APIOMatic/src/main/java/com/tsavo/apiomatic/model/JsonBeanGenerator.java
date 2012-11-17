b9a0808b02d815c5742aceef7589c1be528781fa
Travis Savo <evilgenius@nefariousplan.com>
2012-11-17 15:30:08 -0800

    Lots of code cleanup!

--- APIOMatic/src/main/java/com/tsavo/apiomatic/model/JsonBeanGenerator.java
+++ APIOMatic/src/main/java/com/tsavo/apiomatic/model/JsonBeanGenerator.java
@@ -1,11 +1,11 @@
 package com.tsavo.apiomatic.model;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
 
@@ -30,45 +30,39 @@ import com.sun.codemodel.JVar;
 public class JsonBeanGenerator {
 
 	JCodeModel codeModel = new JCodeModel();
-
+	
 	public static void main(String[] args0) throws Exception {
 		JsonBeanGenerator g = new JsonBeanGenerator();
 		g.test();
 	}
 
 	public void test() throws Exception {
-
-		InputStreamReader input = new InputStreamReader(System.in);
-		BufferedReader reader = new BufferedReader(input);
-
-		// read in user input
-		String json = null;
-
-		json = reader.readLine();
 		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode node = objectMapper.readValue(json, JsonNode.class);
-		JsonNode classNode = node.get("__class__");
-		makeClassFromJson(classNode.textValue(), json, new File("c:\\tmp"));
-
+		makeClassFromJson(objectMapper.readValue(System.in, JsonNode.class), new File("c:\\tmp"));
 	}
 
-	public void makeClassFromJson(String aClassName, JsonNode aNode, File aDestination) throws JClassAlreadyExistsException, IOException {
-		makeClass(aClassName, aNode);
+	public JDefinedClass makeClassFromJson(JsonNode aNode, File aDestination) throws JClassAlreadyExistsException, IOException {
+		return makeClassFromJson(aNode.get("__class__").textValue(), aNode, aDestination);
+	}
+
+	public JDefinedClass makeClassFromJson(String aClassName, JsonNode aNode, File aDestination) throws JClassAlreadyExistsException, IOException {
+		JDefinedClass myClass = makeClass(aClassName, aNode);
 		codeModel.build(aDestination);
+		return myClass;
 	}
 
-	public void makeClassFromJson(String aClassName, String aJsonString, File aDestination) throws JsonParseException, JsonMappingException, IOException, JClassAlreadyExistsException {
+	public JDefinedClass makeClassFromJson(String aClassName, String aJsonString, File aDestination) throws JsonParseException, JsonMappingException, IOException, JClassAlreadyExistsException {
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode node = objectMapper.readValue(aJsonString, JsonNode.class);
-		makeClassFromJson(aClassName, node, aDestination);
+		return makeClassFromJson(aClassName, node, aDestination);
 	}
 
 	public JDefinedClass makeClass(String aClassName, JsonNode aNode) throws JClassAlreadyExistsException {
 		Iterator<Entry<String, JsonNode>> i = aNode.fields();
-		JDefinedClass newClass = codeModel._class(WordUtils.capitalize(sanitizeName(aClassName)));
+		JDefinedClass newClass = codeModel._class(WordUtils.capitalize(aClassName));
 		while (i.hasNext()) {
 			Entry<String, JsonNode> entry = i.next();
-			addBeanField(newClass, entry.getKey(), determineType(entry.getValue(), entry.getKey(), WordUtils.capitalize(sanitizeName(aClassName))));
+			addBeanField(newClass, entry.getKey(), determineType(entry.getValue(), entry.getKey(), WordUtils.capitalize(aClassName)));
 		}
 		return newClass;
 	}
@@ -79,19 +73,19 @@ public class JsonBeanGenerator {
 
 	public JClass determineType(JsonNode aNode, String aName, String aParentName) throws JClassAlreadyExistsException {
 		if (aNode.isInt()) {
-			return codeModel.ref(int.class);
+			return codeModel.ref(Integer.class);
 		}
 		if (aNode.isTextual()) {
 			return codeModel.ref(String.class);
 		}
 		if (aNode.isBoolean()) {
-			return codeModel.ref(boolean.class);
+			return codeModel.ref(Boolean.class);
 		}
 		if (aNode.isLong()) {
-			return codeModel.ref(long.class);
+			return codeModel.ref(Long.class);
 		}
 		if (aNode.isNumber()) {
-			return codeModel.ref(long.class);
+			return codeModel.ref(Long.class);
 		}
 		if (aNode.isArray()) {
 			JClass list = codeModel.ref(List.class);
@@ -102,24 +96,21 @@ public class JsonBeanGenerator {
 			return list;
 		}
 		if (aNode.isObject()) {
-			return makeClass(aParentName + WordUtils.capitalize(sanitizeName(aName)), aNode);
+			String name = aParentName + WordUtils.capitalize(sanitizeName(aName));
+			if(aNode.has("__class__")){
+				name = aNode.get("__class__").textValue();
+			}
+			return makeClass(name, aNode);
 		}
 		throw new RuntimeException("Couldn't determine the type of " + aNode);
 	}
 
 	public static String sanitizeName(String aString) {
-		return aString.replaceAll("\\_", "").replaceAll("class", "clazz");
+		return WordUtils.uncapitalize(aString.replaceAll("\\_", "").replaceAll("class", "clazz"));
 	}
 
-	public static void addBeanField(JDefinedClass aClass, String aName, Class<?> aType) {
-		JFieldVar field = aClass.field(JMod.PRIVATE, aType, sanitizeName(aName));
-		JAnnotationUse property = field.annotate(JsonProperty.class);
-		property.param("value", aName);
-		JMethod m = aClass.method(JMod.PUBLIC, aType, "get" + WordUtils.capitalize(sanitizeName(aName)));
-		m.body()._return(field);
-		JMethod setter = aClass.method(JMod.PUBLIC, void.class, "set" + WordUtils.capitalize(sanitizeName(aName)));
-		JVar var = setter.param(aType, "a" + WordUtils.capitalize(sanitizeName(aName)));
-		setter.body().assign(JExpr._this().ref(field), var);
+	public void addBeanField(JDefinedClass aClass, String aName, Class<?> aType) {
+		addBeanField(aClass, aName, codeModel.ref(aType));
 	}
 
 	public static void addBeanField(JDefinedClass aClass, String aName, JClass aType) {
