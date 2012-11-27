b6dbbb817fe2dfd7e5816f033410c2215edb7776
Devanand Reddy <devananda.reddy@calsoftlabs.com>
2012-11-26 18:48:50 -0800

    Unenhanced classes the proxied classes to generate JSON documentation

--- APIOMatic/src/main/java/com/cpn/apiomatic/controller/AbstractServiceDescriptionController.java
+++ APIOMatic/src/main/java/com/cpn/apiomatic/controller/AbstractServiceDescriptionController.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
 
+import net.sf.cglib.proxy.Enhancer;
+
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,10 +25,15 @@ public abstract class AbstractServiceDescriptionController {
 
 	@RequestMapping(method = { RequestMethod.GET, RequestMethod.OPTIONS })
 	public @ResponseBody
-	List<ServiceDescription> getServiceList() {
+	List<ServiceDescription> getServiceList() throws ClassNotFoundException {
 		List<ServiceDescription> results = new ArrayList<>();
 		for (Entry<String, Object> e : context.getBeansWithAnnotation(Controller.class).entrySet()) {
-			results.add(new ServiceDescription(e.getKey(), e.getValue().getClass()));
+			Class<?> c = e.getValue().getClass();
+			if (ClassUtils.isCglibProxyClass(c)) {
+				c = ClassUtils.getUserClass(c);
+			}
+
+			results.add(new ServiceDescription(e.getKey(), c));
 		}
 		return results;
 	}
@@ -32,7 +41,11 @@ public abstract class AbstractServiceDescriptionController {
 	@RequestMapping(value = "/{name}", method = { RequestMethod.GET, RequestMethod.OPTIONS })
 	public @ResponseBody
 	RestController getRestControllerForClass(@PathVariable("name") String aName) throws ClassNotFoundException {
-		return new RestController(context.getBean(aName).getClass());
+		Class<?> c = context.getBean(aName).getClass();
+		if (ClassUtils.isCglibProxyClass(c)) {
+			c = ClassUtils.getUserClass(c);
+		}
+		return new RestController(c);
 	}
 
 }
